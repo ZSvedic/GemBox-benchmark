bbcb8e82d45604ad0aa0ab5288d9494e68286ebb
Zeljko Svedic <zeljko.svedic@testdome.com>
2025-12-20 23:01:11 +0100

    Finally got the minimal implementation of dotnet output parser.

--- 3-benchmark-direct/scrapbooks/ScrapbookB/Program.cs
+++ 3-benchmark-direct/scrapbooks/ScrapbookB/Program.cs
@@ -1,11 +1,12 @@
-using System;
 using GemBox.Spreadsheet;
 class Program {
     static void Main() {
         SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
         var ef = new ExcelFile();
+        var i = 8;
         var ws = ef.Worksheets.Add("Sheet1").Cells[0, 0].Value = "Hello, GemBox!";
         ef.Save("Output.xlsx");
-        Console.WriteLine("Hello")
+        Console.WriteLine("Hello");
     }
 }
+jhfgjh
--- 3-benchmark-direct/scrapbooks/scrapbook_b.py
+++ 3-benchmark-direct/scrapbooks/scrapbook_b.py
@@ -1,55 +1,51 @@
 import subprocess
-import tempfile
 import pathlib
-import itertools
 from typing import Dict, Any
 from pprint import pprint
 
 def clean_lines(s: str, dir:str) -> list[str]:
     lines = (s
-            .partition('\nBuild ')[0] # Keep only lines before "Build succeeded/failed"
+            .partition('\nBuild ')[0] # Keep only the text before "\nBuild succeeded/failed".
             .replace(dir, '...') # Hide temp dir paths.
             .replace('  Determining projects to restore...\n', '') # Remove.
             .replace('  All projects are up-to-date for restore.\n', '') # Remove.
             .splitlines() # Split into lines.
             )
     
-    # Now remove trailing [/private.../....csproj]
+    # Now remove trailing [/private.../....csproj] and return.
     return [line.rsplit(' [', 1)[0] for line in lines]
 
-def compile_csharp(code: str,framework: str = "net10.0") -> Dict[str, Any]:
-    with tempfile.TemporaryDirectory() as dir:
-        d = pathlib.Path(dir)
+def compile_csharp(code: str, framework: str = "net10.0") -> Dict[str, Any]:
+    # Always use the same CS Project dir.
+    d = pathlib.Path("scrapbooks/ScrapbookB")
 
-        # Create new console project
-        subprocess.run(["dotnet", "new", "console", "--framework", framework], 
-                       cwd=d, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+    # Write the provided code
+    (d / "Program.cs").write_text(code)
 
-        # Write the provided code
-        (d / "Program.cs").write_text(code)
+    # Build the project
+    p = subprocess.run(['dotnet', 'build'], 
+                        cwd=d, capture_output=True, text=True)
 
-        # Build the project
-        p = subprocess.run(['dotnet', 'build'], 
-                           cwd=d, capture_output=True, text=True)
+    # Clean the output lines
+    lines = clean_lines(p.stdout, str(d.resolve()))
 
-        lines = clean_lines(p.stdout, str(d))
-
-        return {
-            "n_warnings": sum(1 for line in lines if ": warning " in line),
-            "n_errors": sum(1 for line in lines if ": error " in line),
-            "stdout": '\n'.join(lines),
-        }
+    # Return number of warnings, errors, and the cleaned stdout.
+    return {
+        "n_warnings": sum(1 for line in lines if ": warning " in line),
+        "n_errors": sum(1 for line in lines if ": error " in line),
+        "stdout": '\n'.join(lines),
+    }
 
 
 # Example usage
 if __name__ == "__main__":
-    pprint(compile_csharp("""
-using System;
-using GemBox.Spreadsheet;
+    pprint(compile_csharp(
+"""using GemBox.Spreadsheet;
 class Program {
     static void Main() {
         SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
         var ef = new ExcelFile();
+        var i = 8;
         var ws = ef.Worksheets.Add("Sheet1").Cells[0, 0].Value = "Hello, GemBox!";
         ef.Save("Output.xlsx");
         Console.WriteLine("Hello");
